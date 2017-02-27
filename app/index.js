62c36d81a21d752c4881da247d7da2c7fb9d0c29
Jhonny Michel <jhonnymichel@outlook.com>
2017-02-27 01:25:00 -0300

    testing ticker

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('rapaz e deu certo ó');
+import Ticker from './js/time/core/Ticker.js';
+
+const ticker = new Ticker({
+  tickInterval: 1000,
+  callback: onFinished,
+});
+
+document.body.onclick = function() {
+  ticker.start();
+};
+
+function onFinished() {
+  console.log('TICK');
+}
