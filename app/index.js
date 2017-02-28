46b8549f0c1f625b605e617f9214f2127b1dfa17
Jhonny Michel <jhonnymichel@outlook.com>
2017-02-28 01:13:37 -0300

    format hours

--- app/index.js
+++ app/index.js
@@ -1,3 +1,9 @@
 import Clock from './js/time/Clock.js';
 
-const clock = new Clock(date => document.body.innerHTML = date.toTimeString());
+const clock = new Clock(onCycle);
+
+function onCycle(date) {
+  const hours = date.toTimeString().split(' ')[0]
+    .substring(0, 5);
+  document.body.innerHTML = hours;
+}
